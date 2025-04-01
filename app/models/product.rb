f67e21870d8ff6d2ef05fd72b8a6d48852990df9
Veer Keshra <veerkeshra9@gmail.com>
2025-04-01 17:46:51 -0500

    Add all model files with associations and validations based on ERD

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :variants, dependent: :destroy
+  has_many :price_histories, dependent: :destroy
+
+  validates :name, :price, :category, presence: true
 end
